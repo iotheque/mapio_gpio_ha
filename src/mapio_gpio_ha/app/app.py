e4b9196b321115bf25a0c3e42672f18e4a848a26
Pierrick <pierrickcurt@gmail.com>
2024-02-23 15:34:46 +0100

    feat: add the linky feature

--- src/mapio_gpio_ha/app/app.py
+++ src/mapio_gpio_ha/app/app.py
@@ -6,16 +6,107 @@ import os
 from pathlib import Path
 from typing import Any
 
-import gpiod  # type: ignore
+import gpiod
+import serial
 from ha_mqtt.ha_device import HaDevice
-from ha_mqtt.mqtt_device_base import MqttDeviceSettings
+from ha_mqtt.mqtt_device_base import MqttDeviceBase, MqttDeviceSettings
 from ha_mqtt.mqtt_sensor import MqttSensor
 from ha_mqtt.mqtt_switch import MqttSwitch
 from ha_mqtt.util import HaDeviceClass
-from paho.mqtt.client import Client  # type: ignore
+from paho.mqtt.client import Client
 
 # Pin definition
 RELAY1_CTRL_PIN = 25
+
+# Exposed linky measures to HA
+# "ACTIVE_REGISTER_TIER_DELIVERED"
+HA_LINKY_MEASURES: list[dict[str, Any]] = [
+    {
+        "unit": "W",
+        "device_class": HaDeviceClass.APPARENT_POWER,
+        "state_class": "measurement",
+        "exposed_variable": "apparent_power",
+    },
+    {
+        "unit": "Wh",
+        "device_class": HaDeviceClass.ENERGY,
+        "state_class": "total_increasing",
+        "exposed_variable": "current_summ_delivered",
+    },
+    {
+        "unit": "Wh",
+        "device_class": HaDeviceClass.ENERGY,
+        "state_class": "total_increasing",
+        "exposed_variable": "current_tier1_summ_delivered",
+    },
+    {
+        "unit": "Wh",
+        "device_class": HaDeviceClass.ENERGY,
+        "state_class": "total_increasing",
+        "exposed_variable": "current_tier2_summ_delivered",
+    },
+]
+
+# /* 0x0000 */ 'currentSummDelivered',
+# /* 0x0001 */ 'currentSummReceived',
+# /* 0x0020 */ 'activeRegisterTierDelivered',
+# /* 0x0100 */ 'currentTier1SummDelivered',
+# /* 0x0102 */ 'currentTier2SummDelivered',
+# /* 0x0104 */ 'currentTier3SummDelivered',
+# /* 0x0106 */ 'currentTier4SummDelivered',
+# /* 0x0108 */ 'currentTier5SummDelivered',
+# /* 0x010A */ 'currentTier6SummDelivered',
+# /* 0x010C */ 'currentTier7SummDelivered',
+# /* 0x010E */ 'currentTier8SummDelivered',
+# /* 0x0110 */ 'currentTier9SummDelivered',
+# /* 0x0112 */ 'currentTier10SummDelivered',
+# /* 0x0307 */ 'siteId',
+# /* 0x0308 */ 'meterSerialNumber',
+# Define the Linky read registers
+LINKY_REGISTERS: list[dict[str, str]] = [
+    {"name": "BASE", "exposed_variable": "current_summ_delivered"},
+    {"name": "PAPP", "exposed_variable": "apparent_power"},
+    {"name": "SINSTS", "exposed_variable": "apparent_power"},
+    {"name": "SINSTS1", "exposed_variable": "apparent_power"},
+    {"name": "HCHC", "exposed_variable": "current_tier1_summ_delivered"},
+    {"name": "EASF01", "exposed_variable": "current_tier1_summ_delivered"},
+    {"name": "EJPHN", "exposed_variable": "current_tier1_summ_delivered"},
+    {"name": "BBRHCJB", "exposed_variable": "current_tier1_summ_delivered"},
+    {"name": "HCHP", "exposed_variable": "current_tier2_summ_delivered"},
+    {"name": "EASF02", "exposed_variable": "current_tier2_summ_delivered"},
+    {"name": "EJPHPM", "exposed_variable": "current_tier2_summ_delivered"},
+    {"name": "BBRHPJB", "exposed_variable": "current_tier2_summ_delivered"},
+]
+
+
+class MqttTicSensor(MqttDeviceBase):
+    """Create the MQTT TIC sensor.
+
+    Args:
+        MqttDeviceBase: herit from MqttDeviceBase
+    """
+
+    device_type = "sensor"
+
+    def __init__(
+        self,
+        settings: MqttDeviceSettings,
+        unit: str,
+        device_class: HaDeviceClass,
+        state_class: str,
+        send_only: bool = False,
+    ):
+        """Create sensor instance."""
+        self.device_class: Any = device_class
+        self.unit_of_measurement = unit
+        self.state_class = state_class
+        super().__init__(settings, send_only)
+
+    def pre_discovery(self) -> None:
+        """Prediscovered function for MqttTicSensor."""
+        self.add_config_option("device_class", self.device_class.value)
+        self.add_config_option("unit_of_measurement", self.unit_of_measurement)
+        self.add_config_option("state_class", self.state_class)
 
 
 # callbacks for the on and off actions
@@ -92,12 +183,13 @@ class MAPIO_GPIO:
         self.relay1_ctrl = chip.get_line(RELAY1_CTRL_PIN)
         self.relay1_ctrl.request(config)
 
-    def expose_mapio_gpio_to_ha(self) -> None:
+    def expose_mapio_gpio_to_ha(self, linky: bool = False) -> None:
         """Expose the desired GPIO to HA."""
         # instantiate an paho mqtt client and connect to the mqtt server
         self.client = Client("mapio-gpio-ha")
         self.client.connect("localhost", 1883)
         self.client.loop_start()
+        self.linky_enable = linky
 
         # create device info dictionary
         dev = HaDevice("MapioGPIO", "mapio-gpio-769251")
@@ -124,11 +216,28 @@ class MAPIO_GPIO:
 
         # instantiate an MQTTDevice object for ANA0 input
         self.ups = MqttSensor(
-            MqttDeviceSettings("UPS Voltage", "ups", self.client),
+            MqttDeviceSettings("UPS Voltage", "ups", self.client, dev),
             "%",
             HaDeviceClass.BATTERY,
             True,
         )
+
+        if self.linky_enable:
+            self.linky: dict[str, Any] = {}
+            for measure in HA_LINKY_MEASURES:
+                self.linky[measure["exposed_variable"]] = MqttTicSensor(
+                    MqttDeviceSettings(
+                        f"Teleinfo {measure['exposed_variable']}",
+                        measure["exposed_variable"],
+                        self.client,
+                        dev,
+                    ),
+                    measure["unit"],
+                    measure["device_class"],
+                    measure["state_class"],
+                    True,
+                )
+                self.linky[measure["exposed_variable"]].pre_discovery()
 
     def refresh_mapio_gpio_to_ha(self) -> None:
         """Function that refresh values to send to HA."""
@@ -166,6 +275,33 @@ class MAPIO_GPIO:
 
         self.ups.publish_state(percent)
 
+    def read_teleinfo(self, port: str = "/dev/ttyAMA3", baudrate: int = 1200) -> None:
+        """Task that read teleinfo from Linky."""
+        serial_port = serial.Serial(
+            port,
+            baudrate,
+            bytesize=serial.SEVENBITS,
+            parity=serial.PARITY_EVEN,
+            stopbits=serial.STOPBITS_ONE,
+        )
+        try:
+            while True:
+                try:
+                    line = serial_port.readline().decode("utf-8").strip()
+                    parsed_data = {}
+                    for register in LINKY_REGISTERS:
+                        if line.startswith(register["name"]):
+                            parsed_data[register["name"]] = line.split(" ")[1]
+                            self.logger.info(f"parsed_data {parsed_data}")
+                            self.linky[register["exposed_variable"]].publish_state(
+                                parsed_data[register["name"]]
+                            )
+                except UnicodeDecodeError:
+                    self.logger.warn("Wrong formatted line received")
+
+        except KeyboardInterrupt:
+            serial_port.close()
+
     def close_mapio_gpio_to_ha(self) -> None:
         """Free the allocated resources."""
         # close the device for cleanup. Gets marked as offline/unavailable in homeassistant
@@ -174,6 +310,10 @@ class MAPIO_GPIO:
 
         self.relay1.close()  # type: ignore
         self.ups.close()  # type: ignore
+        if self.linky_enable:
+            for measure in HA_LINKY_MEASURES:
+                self.linky[measure["exposed_variable"]].close()
+
         self.led_r.close()  # type: ignore
         self.led_g.close()  # type: ignore
         self.led_b.close()  # type: ignore
